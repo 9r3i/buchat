cb504e9a318e49430429050d7e2bde4828f0e693
9r3i <luthfie@y7mail.com>
2025-06-07 03:27:42 +0700

    add fonts

---
+++ fonts/base.js
@@ -0,0 +1,19 @@
+<!-- raw.offline, authored by 9r3i, https://github.com/9r3i, started at april 25th 2018 -->
+<!DOCTYPE html><html lang="en-US" dir="ltr"><head>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+  <title>Silsilah</title>
+  <meta name="robots" content="no follow,no index" />
+  <meta name="author" content="9r3i" />
+  <meta name="uri" content="https://github.com/9r3i" />
+  <meta name="generator" content="\9r3i\mobile" />
+  <meta name="version" content="1.1.0" />
+<script type="text/javascript" src="cordova.js"></script>
+</head><body style="margin:0px;padding:0px;overflow:hidden;">
+<div style="display:flex;height:100vh;align-items:center;justify-content:center;background-color:#031b4e;padding:50px;" id="splash-screen">
+<img src="images/icon.png" style="max-width:100%;max-height:100%;" />
+</div>
+<script type="text/javascript">__RAW_SCRIPT__</script>
+</body></html>
+<!-- powered by 9r3i -->
